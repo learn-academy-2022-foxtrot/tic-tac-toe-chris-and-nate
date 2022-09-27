6485a77b42fe48f803a0e32921679a3525c09ec4
c.r.macias56@hotmail.com <c.r.macias56@hotmail.com>
2022-09-27 13:19:37 -0700

    need draw function to work

--- src/App.js
+++ src/App.js
@@ -4,20 +4,51 @@ import './App.css'
 
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
-  const [player, setPlayer] = useState("❌")
-  console.log(squares)
+  const [player, setPlayer] = useState(0)
+  const [counter, setCounter] = useState(0)
+  //console.log(squares)
   const handleGamePlay = (squareIndex) => {
     let updateBoard = [...squares]
-    if(squares[squareIndex] === null) {
-      updateBoard[squareIndex] = player
+
+    if(player % 2 === 0 && updateBoard[squareIndex] !== "❌" && updateBoard[squareIndex]!=="⭕️") {
+      updateBoard[squareIndex]= "⭕️" 
       setSquares(updateBoard)
-    } else if(squareIndex)  {
-      updateBoard[squareIndex] = ""
-    } else {
-    updateBoard[squareIndex] = ""
-    }
+      setPlayer(player + 1)
+      setCounter(counter + 1)
+    }  else if(player % 2 !== 0 && updateBoard[squareIndex] !== "⭕️" && updateBoard[squareIndex]!=="❌") {
+      updateBoard[squareIndex]= "❌" 
+      setSquares(updateBoard)
+      setPlayer(player + 1)
+      setCounter(counter + 1)
+  }
   }
 
+    let winner = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    for (let i = 0; i < winner.length; i++) {
+      const [a, b, c] = winner[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        alert ("You Win") 
+      } 
+    }
+      if(counter === squares.length){
+          alert ("DRAW")
+        }
+    
+  
+
+  const handleReset = () => {
+    setSquares(Array(9).fill(null))
+  }
+  
   
   //   let boardUpdate = [...squares]
   //   boardUpdate[index] = "❌"
@@ -31,7 +62,9 @@ const App = () => {
 
   return (
     <>
+    <body>
       <h1>Tic Tac Toe</h1>
+      <br />
       <div className='board'>
       {squares.map((squares, index) => {
           return (
@@ -44,6 +77,11 @@ const App = () => {
           )
       })}    
       </div>
+      <br />
+      <div className="button">
+      <button onClick={handleReset}>Restart Game</button>
+      </div>
+      </body>
     </>
   )
 }
