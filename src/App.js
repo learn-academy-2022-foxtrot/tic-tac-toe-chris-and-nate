003832414856a3871c9f6be472b669b9a1e37ceb
c.r.macias56@hotmail.com <c.r.macias56@hotmail.com>
2022-09-30 11:57:10 -0700

    COMPLETED

--- src/App.js
+++ src/App.js
@@ -6,23 +6,30 @@ const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
   const [player, setPlayer] = useState(0)
   const [counter, setCounter] = useState(0)
+  const [gamePlay, setGamePlay] = useState(true)
+  const [message, setMessage] = useState("")
   //console.log(squares)
   const handleGamePlay = (squareIndex) => {
     let updateBoard = [...squares]
 
-    if(player % 2 === 0 && updateBoard[squareIndex] !== "❌" && updateBoard[squareIndex]!=="⭕️") {
-      updateBoard[squareIndex]= "⭕️" 
+    if (player % 2 === 0 && updateBoard[squareIndex] !== "❌" && updateBoard[squareIndex] !== "⭕️") {
+      updateBoard[squareIndex] = "⭕️"
       setSquares(updateBoard)
       setPlayer(player + 1)
       setCounter(counter + 1)
-    }  else if(player % 2 !== 0 && updateBoard[squareIndex] !== "⭕️" && updateBoard[squareIndex]!=="❌") {
-      updateBoard[squareIndex]= "❌" 
+
+    } else if (player % 2 !== 0 && updateBoard[squareIndex] !== "⭕️" && updateBoard[squareIndex] !== "❌") {
+      updateBoard[squareIndex] = "❌"
       setSquares(updateBoard)
       setPlayer(player + 1)
       setCounter(counter + 1)
-  }
+
+    }
+
   }
 
+
+  let isWinner = () => {
     let winner = [
       [0, 1, 2],
       [3, 4, 5],
@@ -33,56 +40,68 @@ const App = () => {
       [0, 4, 8],
       [2, 4, 6],
     ]
+
     for (let i = 0; i < winner.length; i++) {
       const [a, b, c] = winner[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        alert ("You Win") 
-      } 
+
+        return squares[a]
+
+      }
+
+
     }
-      if(counter === squares.length){
-          alert ("DRAW")
-        }
-    
-  
+    return null
+  }
+
+  if (gamePlay) {
+    if (isWinner(squares) === "❌") {
+      console.log("X is winner")
+      setMessage(<h1>❌ WINS!</h1>)
+      setGamePlay(false)
+    } else if (isWinner(squares) === "⭕️") {
+      console.log("⭕️ is the winner")
+      setMessage(<h1>⭕️ WINS!</h1>)
+      setGamePlay(false)
+    } else if(squares.indexOf(null) === -1){
+      console.log("no turn/ draw")
+      setMessage(<h1>DRAW!</h1>)
+      setGamePlay(false)
+    }
+
+  }
+
+
 
   const handleReset = () => {
     setSquares(Array(9).fill(null))
+    setMessage("")
+    setGamePlay(true)
   }
-  
-  
-  //   let boardUpdate = [...squares]
-  //   boardUpdate[index] = "❌"
-  //   setSquares(boardUpdate)
-
-  // }
-// const handleGamePlay = (clickedSquare) => {
-//   let updatedBoard = [...board]
-//   if(clickedSquare !== )
-// }
 
   return (
-    <>
-    <body>
+    <div className="body">
+
       <h1>Tic Tac Toe</h1>
       <br />
       <div className='board'>
-      {squares.map((squares, index) => {
+        {message}
+        {squares.map((squares, index) => {
           return (
-          <Square
-          squares={squares}
-          index={index}
-          key={index}
-          handleGamePlay={handleGamePlay}
-          />
+            <Square
+              squares={squares}
+              index={index}
+              key={index}
+              handleGamePlay={handleGamePlay}
+            />
           )
-      })}    
+        })}
       </div>
       <br />
       <div className="button">
-      <button onClick={handleReset}>Restart Game</button>
+        <button onClick={handleReset}>Restart Game</button>
       </div>
-      </body>
-    </>
+    </div>
   )
 }
 
